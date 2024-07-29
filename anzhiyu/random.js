ee58fc01dad5d9b49f8c19f76f87f13bbb8590cc
Borring1998 <Borring1998@163.com>
2024-07-29 23:15:00 +0800

    Site updated: 2024-07-29 23:14:58

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/bd0a9b9.html","posts/15edad24.html","posts/f782f02c.html","posts/5ffa2476.html","posts/6b7df25a.html","posts/eb44505a.html","posts/23df3862.html","posts/7f46148a.html","posts/1ded83c4.html","posts/7a6a6b5a.html","posts/2f8df7a1.html","posts/4a17b156.html"];function toRandomPost(){
+var posts=["posts/bd0a9b9.html","posts/15edad24.html","posts/f782f02c.html","posts/eb44505a.html","posts/23df3862.html","posts/7f46148a.html","posts/1ded83c4.html","posts/7a6a6b5a.html","posts/4a17b156.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
