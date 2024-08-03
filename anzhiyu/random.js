68360953b7e2822299f875ce4e10d5dc4c085cc4
Boring1998 <Boring1998@163.com>
2024-08-03 18:08:43 +0800

    Site updated: 2024-08-03 18:08:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/bd0a9b9.html","posts/4789a9b5.html","posts/15edad24.html","posts/f782f02c.html","posts/eb44505a.html","posts/23df3862.html","posts/7f46148a.html","posts/1ded83c4.html","posts/7a6a6b5a.html","posts/4a17b156.html"];function toRandomPost(){
+var posts=["posts/bd0a9b9.html","posts/3026.html","posts/4789a9b5.html","posts/15edad24.html","posts/f782f02c.html","posts/eb44505a.html","posts/23df3862.html","posts/7f46148a.html","posts/1ded83c4.html","posts/7a6a6b5a.html","posts/4a17b156.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
