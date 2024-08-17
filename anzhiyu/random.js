5be2589a55b7a2465ec3fbaaa9c344b17f68cefd
Boring1998 <Boring1998@163.com>
2024-08-18 07:15:58 +0800

    Site updated: 2024-08-18 07:15:54

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["posts/bd0a9b9.html","posts/1ded83c4.html","posts/e3639515.html","posts/cecd97f1.html","posts/4e3ae82b.html","posts/3026.html","posts/4789a9b5.html","posts/15edad24.html","posts/f782f02c.html","posts/eb44505a.html","posts/23df3862.html","posts/7f46148a.html","posts/7a6a6b5a.html","posts/4a17b156.html"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
